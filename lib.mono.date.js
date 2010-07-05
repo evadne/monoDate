139f3cf89eff09293ae0b7a260ca88668dda1731
Evadne Wu <ev@monoceroi.com>
2010-07-05 16:53:09 +0800

    migrates to $.each from the buggy for-in loop

--- lib.mono.date.js
+++ lib.mono.date.js
@@ -5,112 +5,138 @@
 
 
 
-String.prototype.clone = function() {
-	
-	return this.slice(0);
-	
-}
 
 
 
 
 
-String.prototype.padToLengthWithPaddingString = function(destinationLength, paddingString) {
+//	Helpers
 
-	if (!destinationLength) return String(this);
-	paddingString = paddingString || "0";
-
-	if (this.length >= destinationLength)
-	return String(this);
-
-	var finalString = "" + this;
-
-	for (var i = 0; i <= (destinationLength - this.length - 1); i++)
-	finalString = (paddingString + finalString);
-	
-	return finalString;
-	
-}
-
-
-
-
-
-Date.prototype.format = function(formatString) {
-
-	//	e.g. "#{MONTH} / #{DATE}  #{HOURS}:#{MINUTES}";
-	
-	var templates = {
-	
-		"YEAR": this.getFullYear(),
-		"MONTH": this.getMonth() + 1,
-		"DAY": this.getDate(),			//	Why the day is called “date” is beyond imagination
-
-		"HOURS": this.getHours(),
-		"MINUTES": this.getMinutes(),
-		"SECONDS": this.getSeconds()
-	
-	}
-	
-	var responseString = "" + formatString;
-	
-	//	Expressions:	
-	//	#\\{MONTH(?:, )?(?:\\d+)?\\}	in JS strings
-	//	#\{MONTH(?:, )?(?:\d+)?\}	in JS Regexs
-	//	#{MONTH(?:, )?(?:\d+)?}		in actual values, matches particle parts e.g. #{MONTH, 24}, or #{MONTH}.
-	
-	
-	
-	
-	
-	var parseDigitPredicate = function(inputString) {
-	
-		if (!inputString) return undefined;
-		inputString = String(inputString);
-	
-		var pattern = /(\d+)(})/ig;
-	
-		if (inputString.match(pattern) != null)
-		return Number(String(inputString.match(pattern)[0]).replace((pattern), '$1'));
-	
-		return undefined;
+	String.prototype.clone = function() {
+		
+		return this.slice(0);
 		
 	}
-	
-	
-	
-	
-	
-	for (templateItemKey in templates) {
-	
-		if (!templates.hasOwnProperty(templateItemKey)) break;
-		
-		var pattern = new RegExp("(#\\{)(" + templateItemKey + ")(?:, )?(?:\\d+)?(\\})", "ig");
-		
-		templateTagOccurrances = formatString.match(pattern);
-		if (templateTagOccurrances == null) continue;
 
-		for (var templateItemOccurranceKey = 0; templateItemOccurranceKey < templateTagOccurrances.length; templateItemOccurranceKey++) {
-		
-			templateItemOccurranceString = String(templateTagOccurrances[templateItemOccurranceKey]);
-			
-			if (templateItemOccurranceString == '') continue;
-			
-			formatString = formatString.replace(
-			
-				templateItemOccurranceString,
+	String.prototype.padToLengthWithPaddingString = function(destinationLength, paddingString) {
+	
+		if (!destinationLength) return this;
+		if (this.length >= destinationLength) return this;
 
-				String(templates[templateItemKey]).padToLengthWithPaddingString(parseDigitPredicate(templateItemOccurranceString), "0")
-			
-			);
-					
+		if (!paddingString) paddingString = "0";
+	
+		var finalString = this.clone();
+	
+		for (var i = 0; i <= (destinationLength - this.length - 1); i++)
+		finalString = (paddingString + finalString);
+		
+		return finalString;
+		
+	}
+
+
+
+
+
+
+
+
+
+
+//	Formatting
+
+	Date.prototype.format = function(formatString) {
+	
+		//	e.g. "#{MONTH} / #{DATE}  #{HOURS}:#{MINUTES}";
+		
+		var templates = {
+		
+			"YEAR": this.getFullYear(),
+			"MONTH": this.getMonth() + 1,
+			"DAY": this.getDate(),			//	Why the day is called “date” is beyond imagination
+	
+			"HOURS": this.getHours(),
+			"MINUTES": this.getMinutes(),
+			"SECONDS": this.getSeconds()
+		
 		}
+		
+		
+		
+		
+		
+		var responseString = String(formatString);
+		
+		//	Expressions:	
+		//	#\\{MONTH(?:, )?(?:\\d+)?\\}	in JS strings
+		//	#\{MONTH(?:, )?(?:\d+)?\}	in JS Regexs
+		//	#{MONTH(?:, )?(?:\d+)?}		in actual values, matches particle parts e.g. #{MONTH, 24}, or #{MONTH}.
+		
+		
+		
+		
+		
+		var parseDigitPredicate = function(inputString) {
+		
+			if (!inputString) return undefined;
+			
+			var inputString = String(inputString);
+			var pattern = /(\d+)(})/ig;
+			var inputMatches = inputString.match(pattern);
+		
+			if (!inputMatches) return undefined;
+			
+			return parseInt(String(inputMatches[0]).replace(pattern, '$1'));
+		
+			
+		}
+		
+		
+		
+		
+		
+	//	Smoking some Objective-C won’t hurt.  I don’t really remember what “true” means.
+		
+		var kjQueryEachLoopBreakingModeBreakLoop = false;
+		var kjQueryEachLoopBreakingModeContinueNextIteration = true;
+		
+		$.each(templates, function(templateItemKey, templateItemValue) {
+		
+			var pattern = new RegExp("(#\\{)(" + templateItemKey + ")(?:, )?(?:\\d+)?(\\})", "ig");
+			var templateTagOccurrances = formatString.match(pattern);
+			
+			if (!templateTagOccurrances)
+			return kjQueryEachLoopBreakingModeContinueNextIteration;
+			
+			$.each(templateTagOccurrances, function(templateTagOccurranceIndex, templateTagOccurrance) {
+			
+				var templateItemOccurranceString = String(templateTagOccurrance);
+				
+				formatString = formatString.replace(
+				
+					templateItemOccurranceString,
 	
+					String(templateItemValue).padToLengthWithPaddingString(
+					
+						parseDigitPredicate(templateItemOccurranceString), 
+						"0"
+						
+					)
+				
+				);
+				
+			});
+		
+		});
+		
+		return formatString;
+		
 	}
-	
-	return formatString;
-	
-}
+
+
+
+
+
 
 
 
